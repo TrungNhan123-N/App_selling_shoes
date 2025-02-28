175dd4da99cc017b8df1200f85ba556f93d7b70f
letru7 <letru7@gmail.com>
2025-02-28 17:26:04 +0700

    update

---
+++ app_selling_shoes/lib/category.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  final String category;
+
+  CategoryScreen({required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(category)),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.category),
+            title: Text("$category - Shoe Model $index"),
+            subtitle: Text("Details of shoe model $index"),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ app_selling_shoes/lib/home.dart
@@ -0,0 +1,73 @@
+import 'package:app_selling_shoes/category.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  final List<String> categories = ["Sneakers", "Formal", "Casual", "Boots", "Sandals"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Shoe Store')),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Search for shoes...",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CategoryScreen(category: categories[index]),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(8),
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(child: Text(categories[index], style: TextStyle(color: Colors.white))),
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            height: 150,
+            color: Colors.orangeAccent,
+            child: Center(child: Text("Promotional Banner", style: TextStyle(fontSize: 20, color: Colors.white))),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(Icons.shopping_bag),
+                  title: Text("New Shoe Model $index"),
+                  subtitle: Text("Description of shoe model $index"),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
